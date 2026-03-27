468dee7044657b509f64aa035e639f63c86e655e
efolgar-2025403 <efolgar-2025403@kinal.edu.gt>
2026-03-27 11:00:50 -0600

    feat: se agrega el repositorio DetalleVentaRepository

---
+++ src/main/java/com/everfolgar/kinalapp/repository/DetalleVentaRepository.java
@@ -0,0 +1,7 @@
+package com.everfolgar.kinalapp.repository;
+
+import com.everfolgar.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
+}
